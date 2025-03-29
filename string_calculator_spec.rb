5eb7cdcebba74b7e8217fa44970de6f0d929c207
bhagwatkhandekar <bhagwatkhandekar312@gmail.com>
2025-03-29 21:16:18 +0530

    given input is negative number then exception raised

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -22,4 +22,8 @@ describe StringCalculator do
     expect(@calculator.add("//;\n1;2")).to eq(3)
   end
 
+  it "raises an exception for negative numbers" do
+    expect { @calculator.add("1,-2,3,-4") }.to raise_error("Negative numbers not allowed: -2, -4")
+  end
+
 end
